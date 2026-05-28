7953b90d75d44c0a0585d201fd900a14afd93891
RichardDorian <richard.dorian@outlook.com>
2026-05-28 12:51:52 +0200

    feat(deployment): report status

--- cmd/server/models/deployment.go
+++ cmd/server/models/deployment.go
@@ -2,12 +2,20 @@ package models
 
 import "time"
 
+type DeploymentStatus string
+
+const (
+	InProgress DeploymentStatus = "in-progress"
+	Error      DeploymentStatus = "error"
+	Deployed   DeploymentStatus = "deployed"
+)
+
 type Deployment struct {
-	ID        string    `gorm:"primaryKey" json:"id"`
-	ProjectID string    `gorm:"index;not null" json:"projectId"`
-	Status    string    `gorm:"not null" json:"status"`
-	Message   string    `json:"message"`
-	URL       string    `json:"url"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	ID        string           `gorm:"primaryKey" json:"id"`
+	ProjectID string           `gorm:"index;not null" json:"projectId"`
+	Status    DeploymentStatus `gorm:"not null" json:"status"`
+	Message   string           `json:"message"`
+	URL       string           `json:"url"`
+	CreatedAt time.Time        `json:"createdAt"`
+	UpdatedAt time.Time        `json:"updatedAt"`
 }
