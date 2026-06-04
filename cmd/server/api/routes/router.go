08efb59f83aaa660959b9e73bcb6dc01a456f798
RichardDorian <richard.dorian@outlook.com>
2026-06-04 09:28:27 +0200

    fix(server): fix create project route path

--- cmd/server/api/routes/router.go
+++ cmd/server/api/routes/router.go
@@ -39,7 +39,7 @@ func NewRouter() *gin.Engine {
 	projects := router.Group("/v1/projects")
 	projects.Use(middleware.Auth(), middleware.ProjectOwnership())
 
-	projects.POST("/:projectId", CreateProject)
+	projects.POST("", CreateProject)
 	projects.GET("", ListProjects)
 	projects.GET("/:projectId", GetProject)
 	projects.DELETE("/:projectId", DeleteProject)
