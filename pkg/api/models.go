b2bab672992ac80c82a14567ba85cba57b493a80
leo.torres <leo.torres@etu.umontpellier.fr>
2026-06-07 21:21:38 +0200

    feat: add function to list all files

--- pkg/api/models.go
+++ pkg/api/models.go
@@ -68,6 +68,11 @@ type Project struct {
 	UpdatedAt     string `json:"updatedAt"`
 }
 
+// ListProjectsResponse provides a list of projects.
+type ListProjectsResponse struct {
+	Projects []Project `json:"projects"`
+}
+
 // DeployProjectRequest selects a deploy target.
 type DeployProjectRequest struct {
 	Tag    string `json:"tag,omitempty"`
