608f2cc3d9aa66e2eaef06412de223cdf5f95493
RICHARD Dorian <richard.dorian@outlook.com>
2026-06-08 21:54:33 +0200

    chore(traefik): setup production traefik tls provider (#69)

--- cmd/server/deployment/project/traefik.go
+++ cmd/server/deployment/project/traefik.go
@@ -1,14 +1,26 @@
 package project
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
-const traefikNet = "traefik"
+func traefikNet() string {
+	net := os.Getenv("TRAEFIK_NET")
+
+	if net == "" {
+		log.Fatal("Missing TRAEFIK_NET environment variable.")
+	}
+
+	return net
+}
 
 func TraefikLabels(containerName, appName, projectSlug, projectID string) map[string]string {
 	domain := fmt.Sprintf("%s-%s-%s.pushnpray.polydo.dev", appName, projectSlug, projectID)
 	return map[string]string{
 		"traefik.enable":         "true",
-		"traefik.docker.network": traefikNet,
+		"traefik.docker.network": traefikNet(),
 		fmt.Sprintf("traefik.http.routers.%s.rule", containerName):             fmt.Sprintf("Host(`%s`)", domain),
 		fmt.Sprintf("traefik.http.routers.%s.entrypoints", containerName):      "websecure",
 		fmt.Sprintf("traefik.http.routers.%s.tls", containerName):              "true",
